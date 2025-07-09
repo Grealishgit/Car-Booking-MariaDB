2e6fee94293fd9f7a29dc0d2dbd9e55032a54b70
Hunter <eugyneehunter@gmail.com>
2025-07-09 16:33:05 +0300

    Modified the home background image

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,39 +2,40 @@ import React from 'react'
 import car from '../assets/car1.png'
 import { FaCar } from "react-icons/fa";
 import CardCard from '../components/CardCard';
+import interior from '../assets/bg.png'
 
 const Home = () => {
 
     return (
         <>
-            <div className="flex flex-col p-2 items-center  min-h-screen
-         bg-gradient-to-r from-blue-400 via-blue-500 to-blue-700 text-center">
+            <div className="flex flex-col p-2 items-center bg-center bg-cover
+            
+              min-h-screen text-center"
+                style={{ backgroundImage: `url(${interior})` }}>
                 <div className="flex md:flex-row flex-col  items-center justify-center">
-                    <div className="md:w-1/2 w-full mt-20 ">
-                        <p className="text-white md:text-[70px] text-lg font-bold">
-                            Discover Endless Possibilities with  ShikaRide
+                    <div className="w-full mt-20 ">
+                        <p className="text-white text-[80px] textlg font-bold">
+                            Discover Endless <br /> Possibilities with <span className='text-blue-500'>ShikaRide</span> 
                         </p>
 
                         <p className="text-white md:text-[20px] text-md font-semibold mt-2">
                             Your journey starts here. Book your ride now!
                         </p>
                         {/* <div className="flex flex-col items-center mt-8">
-                        <button className="mt-6 flex flex-row cursor-pointer items-center gap-3
+                            <button className="mt-15 flex flex-row cursor-pointer items-center gap-3
                          font-bold px-20 py-3 bg-white text-blue-600 rounded-lg
                      hover:bg-blue-100 transition duration-300">
                             <FaCar />
-                            Book Now
+                                Explore Cars
                         </button>
-                    </div> */}
+                        </div> */}
 
                     </div>
-                    <div className='md:w-1/2 -mt-20 w-full'>
-                        <img src={car} alt="Car" className='w-200 h-auto' />
-                    </div>
+
                 </div>
 
 
-                <div className="flex md:flex-row flex-col  border backdrop-blur-3xl bg-black/40
+                <div className="flex md:flex-row flex-col relative top-30  border backdrop-blur-3xl bg-black/40
              border-gray-300 md:rounded-2xl rounded-lg p-4 justify-center mt-2 mb-4">
 
                     <div className="flex flex-col items-start space-y-2 p-2  rounded-lg  md:w-1/2 w-full">
