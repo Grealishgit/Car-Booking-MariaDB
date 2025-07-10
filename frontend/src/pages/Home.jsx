f98903667cb9ca786aaea76da243a675ecac44ff
Hunter <eugyneehunter@gmail.com>
2025-07-10 15:57:01 +0300

    Add promotional banner with discount offer to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import car from '../assets/car1.png'
 import { FaCar } from "react-icons/fa";
 import CardCard from '../components/CardCard';
@@ -6,13 +6,29 @@ import interior from '../assets/bg.png'
 import logo from '../assets/logoc.png'; 
 
 const Home = () => {
+    const [banner, setBanner] = useState(true);
 
     return (
         <>
+            {banner && (<div class="w-full relative top-16 flex items-center justify-between  md:px-14 py-2
+                 font-medium text-sm text-white text-center bg-gradient-to-r from-violet-500 via-blue-500 to-[#E0724A]">
+                <p>Get 20% OFF on Your First Ride Order!</p>
+                <div class="flex items-center space-x-6">
+                    <button type="button" class="font-normal text-gray-800 bg-white px-7 py-2 rounded-full">Claim Offer</button>
+                    <button className='cursor-pointer' onClick={() => setBanner(!banner)} type="button">
+                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect y="12.532" width="17.498" height="2.1" rx="1.05" transform="rotate(-45.74 0 12.532)" fill="#fff" />
+                            <rect x="12.533" y="13.915" width="17.498" height="2.1" rx="1.05" transform="rotate(-135.74 12.533 13.915)" fill="#fff" />
+                        </svg>
+                    </button>
+                </div>
+            </div>)}
             <div className="flex flex-col p-2 items-center bg-center bg-cover
             
               min-h-screen text-center"
                 style={{ backgroundImage: `url(${interior})` }}>
+
+
                 <div className="flex md:flex-row flex-col  items-center justify-center">
                     <div className="w-full mt-20 ">
                         <p className="text-white md:text-[80px] text-3xl font-bold">
