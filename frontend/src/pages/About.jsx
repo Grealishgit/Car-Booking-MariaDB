f419eaecf695d85c12ed261ff34f5a470b378f88
Hunter <eugyneehunter@gmail.com>
2025-07-11 09:21:57 +0300

    Update About page content and buttons for improved clarity and engagement

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -13,7 +13,8 @@ const About = () => {
                     className="mt-16 mb-6 flex items-center space-x-2 border border-indigo-600 text-indigo-600 text-xs rounded-full px-4 pr-1.5 py-1.5 hover:bg-indigo-50 transition"
                     type="button">
                     <span>
-                        Explore how we help grow brands.
+                        Shika <span className="hidden sm:inline">Ride </span>
+                        <span className="hidden sm:inline">The Prefered Car Marketplace</span>
                     </span>
                     <span className="flex items-center justify-center size-6 p-1 rounded-full bg-indigo-600">
                         <svg width="14" height="11" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -23,30 +24,40 @@ const About = () => {
                     </span>
                 </button>
                 <h1 className="text-gray-900 font-bold text-3xl sm:text-4xl md:text-5xl max-w-xl leading-tight">
-                    Preferred choice of leaders in
-                    <span className="text-indigo-600">
-                        every industry
+                    From comfort to luxury, we have it all for
+                    <span className="text-indigo-600"> every journey
+
                     </span>
                 </h1>
                 <p className="mt-4 text-gray-600 max-w-md text-sm sm:text-base leading-relaxed">
-                    Learn why professionals trust our solution to complete
-                    their customer journey.
+                    We are a leading car marketplace, dedicated to connecting you with the perfect vehicle for your needs.
+                    Whether you're looking for a compact car for city driving or a spacious SUV for family adventures,
+                    we have a wide range of options to suit every taste and budget. Our platform makes it easy to browse,
+                    compare, and find the ideal car that fits your lifestyle.
+                    <br />
+
+                    <span className="hidden sm:inline">With Shika, your next adventure is just a click away.</span>
                 </p>
-                <div className="flex flex-col md:flex-row items-center mt-8 gap-3">
+                <div className="flex flex-col mb-5 md:flex-row items-center mt-8 gap-3">
                     <button
-                        className="bg-indigo-600 text-white px-6 pr-2.5 py-2.5 rounded-full text-sm font-medium flex items-center space-x-2 hover:bg-indigo-700 transition"
+                        className="bg-indigo-600 cursor-pointer text-white px-6 pr-2.5 py-2.5
+                         rounded-full text-sm font-medium flex items-center space-x-2 hover:bg-indigo-700 transition"
                         type="button">
                         <span>
-                            Read Success Stories
+                            Learn More
                         </span>
                         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4.821 11.999h13.43m0 0-6.714-6.715m6.715 6.715-6.715 6.715" stroke="#fff"
                                 stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </button>
-                    <a className="text-indigo-600 bg-indigo-100 px-5 py-2 rounded-full text-sm font-medium hover:bg-indigo-200 transition"
+                    <a className="text-indigo-600 flex items-center bg-indigo-100 px-5 py-2 rounded-full text-sm font-medium hover:bg-indigo-200 transition"
                         href="#">
-                        Get Started
+                        Contact Us
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M4.821 11.999h13.43m0  0-6.714-6.715m6.715 6.715-6.715 6.715" stroke="#6366F1"
+                                stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
                     </a>
                 </div>
             </div>
